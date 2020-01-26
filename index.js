e2c7770943fe027368886ae20e5b3ea6d1c84777
Guiradi <g.ferreira@unesp.br>
2020-01-26 11:55:22 -0300

    Change app name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {name as GasAppName} from './GasApp.json';
 import 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(GasAppName, () => App);
